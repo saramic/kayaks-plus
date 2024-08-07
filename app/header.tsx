f40820fcc6ce51a2c939e5c69a6b230b0f76cb0b
Michael Milewski <saramic@gmail.com>
2024-08-07 12:30:18 +1000

    First go at adding kayaksPlus logo

--- app/header.tsx
+++ app/header.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="has-text-centered">
-      <section className="hero">
+    <header className="has-text-centered pb-4">
+      <section className="hero is-small is-primary">
         <div className="hero-body">
+          <Image
+            src="/logos/kayaksplus_logo.png"
+            alt="kayaksplus logo"
+            width="100"
+            height="100"
+          />
           <Link href="/">
-            <p className="title">kayaksPlus.store</p>
+            <p className="title has-text-white">
+              <span className="has-text-weight-bold">kayaks</span>
+              <span className="has-text-weight-light">plus.store</span>
+            </p>
           </Link>
         </div>
       </section>
