7649631a326634a5532c7e8f6c2317e4449c1a6a
Michael Milewski <saramic@gmail.com>
2024-08-07 14:57:41 +1000

    Add fontawesome

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,10 +2,16 @@ export default function Footer() {
   return (
     <footer className="has-text-centered is-flex-align-items-flex-end mt-auto">
       <p>
-        <strong>KayaksPlus</strong>
+        kayaks
+        <span className="has-text-weight-light">plus.store</span>
+        <span className="icon-text pl-3">
+          <span className="icon is-size-3">
+            <i className="fas fa-earth-oceania"></i>
+          </span>
+        </span>
       </p>
       <small>
-        <span>Copyright @2024</span>
+        <span className="has-text-weight-light">Copyright @2024</span>
         <br />
       </small>
     </footer>
