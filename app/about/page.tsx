fcb23537a8d88c15fa9826f8987d05ce749d2cf4
Michael Milewski <saramic@gmail.com>
2024-08-10 12:44:49 +1000

    Header and styling for about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,17 @@
 export default function Page() {
   return (
-    <main className="container hero is-fullheight">
-      <p>
-        Kayaks Plus is an Australian owned company that specialises in the
-        design and manufacture of canoe polo kayaks and accessories. Our Canoe
-        Polo Kayaks are known around the world for their design, strength,
-        durability and value for money. We regularly export kayaks and
-        accessories around the world and we have test pilots and agents in many
-        countries.
-      </p>
-    </main>
+    <div className="columns is-centered">
+      <div className="column is-half">
+        <h2 className="title">Kayaks Plus</h2>
+        <p>
+          Kayaks Plus is an Australian owned company that specialises in the
+          design and manufacture of canoe polo kayaks and accessories. Our Canoe
+          Polo Kayaks are known around the world for their design, strength,
+          durability and value for money. We regularly export kayaks and
+          accessories around the world and we have test pilots and agents in
+          many countries.
+        </p>
+      </div>
+    </div>
   );
 }
